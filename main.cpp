7bbc1a28c733df04a03f5b8a826f28b0629ce257
borna-tl <borna.tavassoli@gmail.com>
2023-04-26 11:26:32 +0200

    fixed visited array time consumption

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ using namespace std;
 #define BIBFS_MODE 3
 #define SV_MODE 4
 
+//class algorithm with child that handle insertion and queries (like bfs, dfs, etc)
+
 class reachabilityTree{//this is a simple incremental reachability tree for vertex s
 public:
     reachabilityTree(){}
@@ -89,6 +91,9 @@ public:
         input_file(input_file_), output_file(output_file_){
         out_edge.assign(MAX_NODES, vector<int>());
         in_edge.assign(MAX_NODES, vector<int>());
+        visited_bfs.resize(MAX_NODES, false);
+        visited_bibfs_source.resize(MAX_NODES, false);
+        visited_bibfs_sink.resize(MAX_NODES, false);
     }
     void run(int mode){
         //it might be faster to read the file once, instead of keeping it open
@@ -167,29 +172,32 @@ public:
 
 private:
     string input_file, output_file;
-    vector<vector<int>> out_edge; //maybe i should use vector?
-    vector<vector<int>> in_edge; //maybe i should use vector?
+    vector<vector<int>> out_edge;
+    vector<vector<int>> in_edge;
     map<int, bool> visited_dfs;
+    vector<bool> visited_bfs; //in order to reduce resizing cost
+    vector<bool> visited_bibfs_source, visited_bibfs_sink;
     reachabilityTree* reachability_tree[MAX_NODES];
     bool run_bfs(int u, int v){  
-        vector<bool> visited; //maybe this should be map<int, bool>?
-        visited.resize(MAX_NODES, false);
-        queue<int> q;
-        visited[u] = true;
-        q.push(u); 
-        while (!q.empty()) {
-            u = q.front();
-            q.pop();
-            // vector<int>::iterator i;
-
+        vector<int> q;
+        int pointer = 0;
+        visited_bfs[u] = true;
+        q.push_back(u); 
+        while (pointer < q.size()) {
+            u = q[pointer];
+            pointer++;
             for (int i : out_edge[u]){
-                if (!visited[i]){
-                    visited[i] = true;
-                    q.push(i);
+                if (!visited_bfs[i]){
+                    visited_bfs[i] = true;
+                    q.push_back(i);
                 }
             }
         }
-        return visited[v];
+        bool ans = visited_bfs[v];
+        for (int i : q){
+            visited_bfs[i] = false;
+        }
+        return ans;
     }
     bool run_dfs(int u, int v){
         visited_dfs[u] = true;
@@ -204,41 +212,48 @@ private:
         return visited_dfs[v];
     }
     bool run_bibfs(int u, int v){   
-        vector<bool> visited_source, visited_sink; //maybe we should use map<int, bool>
-        visited_source.resize(MAX_NODES, false);
-        visited_sink.resize(MAX_NODES, false);
+        
         bool found_path = false;
-        queue<int> source_queue, sink_queue;
-        visited_source[u] = true;
-        visited_sink[v] = true;
-        source_queue.push(u);
-        sink_queue.push(v);
-        while (!found_path && !source_queue.empty() && !sink_queue.empty()) { //i think if one queue is empty we can terminate
+        vector<int> source_queue, sink_queue;
+        int source_pointer = 0;
+        int sink_pointer = 0;
+        visited_bibfs_source[u] = true;
+        visited_bibfs_sink[v] = true;
+        source_queue.push_back(u);
+        sink_queue.push_back(v);
+        while (!found_path && source_pointer < source_queue.size()
+                            && sink_pointer < sink_queue.size()) {
             //running bfs for the source queue one time
-            u = source_queue.front();
-            source_queue.pop();
+            u = source_queue[source_pointer];
+            source_pointer++;
             vector<int>::iterator i;
             for (i = out_edge[u].begin(); i != out_edge[u].end(); ++i){
-                if (!visited_source[*i]){
-                    visited_source[*i] = true;
-                    source_queue.push(*i);
+                if (!visited_bibfs_source[*i]){
+                    visited_bibfs_source[*i] = true;
+                    source_queue.push_back(*i);
                 }
-                if (visited_source[*i] && visited_sink[*i]){
+                if (visited_bibfs_source[*i] && visited_bibfs_sink[*i]){
                     found_path = true;
                 }
             }
             //running bfs for the back queue one time
-            v = sink_queue.front();
-            sink_queue.pop();
+            v = sink_queue[sink_pointer];
+            sink_pointer++;
             for (i = in_edge[v].begin(); i != in_edge[v].end(); ++i){
-                if (!visited_sink[*i]){
-                    visited_sink[*i] = true;
-                    sink_queue.push(*i);
+                if (!visited_bibfs_sink[*i]){
+                    visited_bibfs_sink[*i] = true;
+                    sink_queue.push_back(*i);
                 }
-                if (visited_source[*i] && visited_sink[*i]){
+                if (visited_bibfs_source[*i] && visited_bibfs_sink[*i]){
                     found_path = true;
                 }
             }
+        }
+        for (int i : source_queue){
+            visited_bibfs_source[i] = false;
+        }
+        for (int i : sink_queue){
+            visited_bibfs_sink[i] = false;
         }
         return found_path;
     }   
@@ -279,8 +294,8 @@ private:
     }
     void update_sv(int u, int v, const vector<int>& sv_list){
         for (auto sv : sv_list){
-            reachability_tree[sv]->update_r_plus(u, v, out_edge); //sample
-            reachability_tree[sv]->update_r_minus(u, v, in_edge); //because it's both ways
+            reachability_tree[sv]->update_r_plus(u, v, out_edge);
+            reachability_tree[sv]->update_r_minus(u, v, in_edge); //remove r_minus run on the reverse graph
         }
     }
 };
